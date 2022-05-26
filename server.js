38c08e2bbdbd88f9511fb7a679bd9acb87552995
Bob Nearents <bobnearents@gmail.com>
2022-05-25 17:28:17 -0700

    add endpoints

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
 dotenv.config();
-import express from "express";
-import { google } from "googleapis";
-import fetch from "node-fetch";
+import express from 'express';
+import { google } from 'googleapis';
+import fetch from 'node-fetch';
 
 const app = express();
 
@@ -10,16 +10,16 @@ const app = express();
 app.use(express.json());
 app.use(function (req, res, next) {
   res.header(
-    "Access-Control-Allow-Origin",
+    'Access-Control-Allow-Origin',
     `${
       process.env.NODE_ENV
-        ? "https://nurture-client.herokuapp.com"
-        : "http://localhost:3000"
+        ? 'https://nurture-client.herokuapp.com'
+        : 'http://localhost:3000'
     }`
   ); // update to match the domain you will make the request from
   res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
   );
   next();
 });
@@ -27,15 +27,15 @@ app.use(function (req, res, next) {
 const jwtClient = new google.auth.JWT(
   process.env.CLIENT_EMAIL,
   null,
-  process.env.PRIVATE_KEY.replace(/\\n/g, "\n"), //sanitize the key since heroku escapes out the \n to \\n https://stackoverflow.com/questions/39492587/escaping-issue-with-firebase-privatekey-as-a-heroku-config-variable
-  ["https://www.googleapis.com/auth/spreadsheets"]
+  process.env.PRIVATE_KEY.replace(/\\n/g, '\n'), //sanitize the key since heroku escapes out the \n to \\n https://stackoverflow.com/questions/39492587/escaping-issue-with-firebase-privatekey-as-a-heroku-config-variable
+  ['https://www.googleapis.com/auth/spreadsheets']
 );
-const sheetsService = google.sheets({ version: "v4", auth: jwtClient });
+const sheetsService = google.sheets({ version: 'v4', auth: jwtClient });
 
 const trimDataTable = (response) => {
   const headers = {
     main: response.data.values[1],
-    fallback: response.data.values[0],
+    fallback: response.data.values[0]
   };
   //"slice" off the headers from the providers array and then begin to loop through each provider
   const providersArray = response.data.values.slice(2);
@@ -46,35 +46,48 @@ const trimDataTable = (response) => {
       contact: {},
       paymentOptions: {},
       services: {},
-      certs: {},
+      certifications: {},
       formData: {},
-      visibility: true,
+      visibility: {}
     };
     headers.main.forEach((header, i) => {
       const headerName = header || headers.fallback[i];
       if (i < 2) newProvider.formData[headerName] = provider[i];
       else if (i < 6) newProvider.services[headerName] = provider[i];
       else if (i < 19) newProvider.contact[headerName] = provider[i];
-      else if (i < 40) newProvider.certs[headerName] = provider[i];
-      else if (i == 40) newProvider.visibility = !provider[i];
-      else if (i == 41 || i == 42)
-        newProvider.visibility = !newProvider.visibility || !!provider[i];
-      //if visibility is already false, keep it that way
-      else if (i < 51) {
-        newProvider.paymentOptions[headerName] = provider[i];
+      else if (i < 40) newProvider.certifications[headerName] = provider[i];
+      else if (i < 43) newProvider.visibility[headerName] = provider[i];
+      else if (i < 51) newProvider.paymentOptions[headerName] = provider[i];
+    });
+    let vis = true;
+
+    Object.values(newProvider.visibility).forEach((param, i) => {
+      if (i === 0) {
+        //if it needs review then make it not visible
+        if (param) vis = false;
+      } else if (
+        param != 'Yes' &&
+        param != 'Currently Practicing' &&
+        param != 'Permission to share'
+      ) {
+        vis = false;
       }
     });
-    return newProvider;
-  });
 
-  return providers;
+    newProvider.visibility = vis;
+    if (vis) return newProvider;
+  });
+  //only send a provider if it should be visible and has a name provided
+  return providers.filter((provider) => {
+    return provider !== undefined && provider.contact['Name'];
+  });
 };
 
-app.get("/providers", async (req, res) => {
+app.get('/providers', async (req, res) => {
   try {
     const sheetsResponse = await sheetsService.spreadsheets.values.get({
       spreadsheetId: process.env.SPREADSHEET_ID,
-      range: process.env.SPREADSHEET_RANGE,
+      range: process.env.SPREADSHEET_RANGE
     });
     const data = trimDataTable(sheetsResponse);
     res.send(data);
@@ -84,7 +97,7 @@ app.get("/providers", async (req, res) => {
   }
 });
 
-app.get("/zip-codes", async (req, res) => {
+app.get('/zip-codes', async (req, res) => {
   const key = process.env.ZIP_KEY;
   const { value, radius } = req.query;
   const rawResult = await fetch(
@@ -94,40 +107,63 @@ app.get("/zip-codes", async (req, res) => {
   res.send(result);
 });
 
-app.post("/comment", (req, res) => {});
-
-app.post("/new-provider", (req, res) => {
-  const newProvider = req.body;
-  Object.keys(newProvider).forEach((key) => {
-    newProvider[key] = Object.values(newProvider[key]);
-  });
-  const visibility = ["yes", "yes", "yes"];
+app.post('/comment', (req, res) => {
   const requestBody = {
-    values: [
-      [
-        ...newProvider.formData,
-        ...newProvider.services,
-        ...newProvider.contact,
-        ...newProvider.certs,
-        ...visibility,
-        ...newProvider.paymentOptions,
-      ],
-    ],
+    values: [[req.body.comment]]
   };
   sheetsService.spreadsheets.values.append(
     {
       spreadsheetId: process.env.SPREADSHEET_ID,
-      range: "A1",
-      valueInputOption: "RAW",
+      range: 'User-Comments!A1',
+      valueInputOption: 'RAW',
       requestBody,
-      insertDataOption: "INSERT_ROWS",
+      insertDataOption: 'INSERT_ROWS'
     },
     (err, result) => {
       if (err) {
         // Handle error.
         console.log(err);
       } else {
-        console.log(result);
+        // console.log(result);
+      }
+    }
+  );
+});
+
+app.post('/new-provider', async (req, res) => {
+  const newProvider = req.body;
+  const formData = [44, 22];
+  //convert object to array
+  Object.keys(newProvider).forEach((key) => {
+    newProvider[key] = Object.values(newProvider[key]);
+  });
+
+  const requestBody = {
+    values: [
+      [
+        ...formData,
+        ...newProvider.services,
+        ...newProvider.contact,
+        ...newProvider.certifications,
+        ...newProvider.visibility,
+        ...newProvider.paymentOptions
+      ]
+    ]
+  };
+  sheetsService.spreadsheets.values.append(
+    {
+      spreadsheetId: process.env.SPREADSHEET_ID,
+      range: 'A1',
+      valueInputOption: 'RAW',
+      requestBody,
+      insertDataOption: 'INSERT_ROWS'
+    },
+    (err, result) => {
+      if (err) {
+        // Handle error.
+        console.log(err);
+      } else {
+        // console.log(result);
       }
     }
   );
