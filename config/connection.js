4439423fe868a92df3a40f344d9aeb29d9a5bfc4
Bob Nearents <bobnearents@gmail.com>
2023-03-30 13:40:56 -0700

    getting closer

--- config/connection.js
+++ config/connection.js
@@ -2,19 +2,11 @@ import pg from 'pg';
 const { Pool } = pg;
 import config from './config.js';
 
-const connection =
-  config.NODE_ENV === 'production'
-    ? {
-        connectionString: config.DATABASE_URL,
-        ssl: {
-          rejectUnauthorized: false
-        }
-      }
-    : {
-        user: config.DB_USERNAME,
-        port: config.DB_PORT,
-        password: config.DB_PASSWORD,
-        database: config.DB_NAME
-      };
+const connection = {
+  user: config.DB_USERNAME,
+  port: config.DB_PORT,
+  password: config.DB_PASSWORD,
+  database: config.DB_NAME
+};
 
 export default new Pool(connection);
