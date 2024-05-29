31d3a4f62f6448dea5f0bddfd88a898eb45e6a18
Bob Nearents <bobnearents@gmail.com>
2024-05-29 13:27:02 -0700

    update emails

--- src/services/aws/ses.js
+++ src/services/aws/ses.js
@@ -61,11 +61,19 @@ const baseUrl =
     ? 'https://earlyparentinghub.org'
     : 'http://localhost:5173';
 
+
+export const sendAdminEmail = async (providerType, id, submissionType) => {
+  const subject = `Somebody has ${submissionType} their information!`
+  const body = `<p>Feel free to <a href='${baseUrl}/${providerType}/${id}'>click here</a> to preview their profile page.</p>
+  <p><a href='${baseUrl}/admin'>click here</a> to approve their information</p>`
+  return sendEmail('directory@nurturerva.org', 'admin@earlyparentinghub.org', body, subject);
+}
 export const sendWelcomeEmail = async (recipient, id, providerType) => {
+  sendAdminEmail(providerType, id, 'submitted')
   const subject = 'Welcome to earlyparentinghub';
   const body = '<p>Thank you for submitting you information to our directory. Your submission will be reviewed shortly, and we will contact you when it is approved or if we have any questions.</p>' +
-    `<p>Feel free to <a href='${baseUrl}/${providerType}/${id}'>click here</a> in the mean time to see a preview of your profile page.
-    <p>Thank you for helping to make access to Richmond’s birth and early parenting resources easier! </p>
+    // `<p>Feel free to <a href='${baseUrl}/${providerType}/${id}'>click here</a> in the mean time to see a preview of your profile page.
+    `<p>Thank you for helping to make access to Richmond’s birth and early parenting resources easier! </p>
     <p>The Nurture Team</p>`;
   return sendEmail(recipient, 'welcome@earlyparentinghub.org', body, subject);
 };
@@ -76,14 +84,15 @@ export const sendRequestEditEmail = async (recipient, userNote, hash, id) => {
   const body = `<p>Someone thinks your information needs to be updated. Here's the note they left: ${userNote}</p>
     <p><a href='${baseUrl}/${id}/edit/${hash}'>Click here</a> to edit your information.</p>
     <p>If you didn't initiate this request, or don't need to update your information, feel free to disregard this email.</p>`;
-  console.log(recipient);
   return sendEmail(recipient, 'help@earlyparentinghub.org', body, subject);
 };
 
 export const sendEditSuccessEmail = async (recipient, providerType, id) => {
+  sendAdminEmail(providerType, id, 'edited')
+
   const subject = 'Your information has been updated';
 
-  const body = `<p>Your information has been updated! <a href='${baseUrl}/${providerType}/${id}'>Click here</a> to preview your changes.</p>`;
+  const body = `<p>Your information has been updated!</p>`;
   return sendEmail(recipient, 'success@earlyparentinghub.org', body, subject);
 };
 
@@ -96,7 +105,6 @@ export const sendBetaLaunchEmail = async (
   const subject = 'Thanks for joining our beta';
 
   const body = `<p><a href='${baseUrl}'>Click here</a> to check out the new resource directory!</p>
-    <p><a href='${baseUrl}/${providerType}/${id}'>Click here</a> to preview your profile with the information you submitted a couple years ago</a></p>
     <p><a href='${baseUrl}/${hash}'>Click here</a> to edit your information. Please update your information and add a photo to your profile, and help us better serve our community.</p>`;
 
   return sendEmail(recipient, 'success@earlyparentinghub.org', body, subject);
